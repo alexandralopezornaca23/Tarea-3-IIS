cf51425bdbc91a1fc44fe0e16e4848b4bdb31254
alexandralopezornaca23 <22249267@live.uem.es>
2025-01-26 06:47:03 +0100

    Controlador de frutas para cambiar de nivel
    Si se recolectan todas las frutas de un nivel se cambia a la siguiente escena en el build settings.

--- Tarea-3/Assets/Scripts/Managers/FruitManager.cs
+++ Tarea-3/Assets/Scripts/Managers/FruitManager.cs
@@ -1,14 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class FruitManager : MonoBehaviour
 {
+    private void Update()
+    {
+        AllFruitsCollected();
+    }
+
     public void AllFruitsCollected()
     {
-        if (transform.childCount == 1)
+        if (transform.childCount == 0)
         {
-            Debug.Log("No quedan frutas, Vistoria!");
+            //Debug.Log("No quedan frutas, Vistoria!");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
     }
 }
